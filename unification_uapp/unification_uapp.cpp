d7b67583c8923bdc467120e01ed39f5c17b9e06a
Codegnosis <paul@codegnosis.co.uk>
2018-09-05 15:42:48 +0100

    temporarily remove custom permissions from smart contract actions

--- unification_uapp/unification_uapp.cpp
+++ unification_uapp/unification_uapp.cpp
@@ -55,7 +55,8 @@ namespace UnificationFoundation {
                                         const signature& sig,
                                         const public_key& pub) {
         print("modifypermsg()");
-        require_auth2(_self,N(modperms));
+        //require_auth2(_self,N(modperms));
+        require_auth(_self);
 
         //Strange.... assert_recover_key only works if recover_key is run first!
         int rec_key = recover_key( (const checksum256 *)&digest, (char *)&sig, sizeof(sig), (char *)&pub, sizeof(pub) );
@@ -103,7 +104,8 @@ namespace UnificationFoundation {
         eosio_assert((schema_vers == 0
                       || schema_vers == 1), "schema_vers must 0 or 1 for dev, prod");
 
-        require_auth2(_self,N(modschema));
+        //require_auth2(_self,N(modschema));
+        require_auth(_self);
 
         unifschemas u_schema(_self, _self);
 
@@ -125,7 +127,8 @@ namespace UnificationFoundation {
                                       const uint8_t& price_adhoc) {
 
         //TODO - migrate to require_auth2 with custom permission level
-        require_auth2(_self,N(modschema));
+       // require_auth2(_self,N(modschema));
+        require_auth(_self);
 
         eosio_assert((schedule == 1
                      || schedule == 2
@@ -151,7 +154,9 @@ namespace UnificationFoundation {
     }
 
     void unification_uapp::setvers(const uint64_t& pkey,const uint8_t& schema_vers) {
-        require_auth2(_self,N(modschema));
+
+        //require_auth2(_self,N(modschema));
+        require_auth(_self);
 
         eosio_assert((schema_vers == 0
                       || schema_vers == 1), "schema_vers must 0 or 1 for dev, prod");
@@ -168,7 +173,8 @@ namespace UnificationFoundation {
     }
 
     void unification_uapp::setschedule(const uint64_t& pkey,const uint8_t& schedule) {
-        require_auth2(_self,N(modschema));
+        //require_auth2(_self,N(modschema));
+        require_auth(_self);
 
         eosio_assert((schedule == 1
                      || schedule == 2
@@ -186,7 +192,8 @@ namespace UnificationFoundation {
     }
 
     void unification_uapp::setpricesch(const uint64_t& pkey,const uint8_t& price_sched) {
-        require_auth2(_self,N(modschema));
+        //require_auth2(_self,N(modschema));
+        require_auth(_self);
 
         unifschemas u_schema(_self, _self);
 
@@ -200,7 +207,8 @@ namespace UnificationFoundation {
     }
 
     void unification_uapp::setpriceadh(const uint64_t& pkey,const uint8_t& price_adhoc) {
-        require_auth2(_self,N(modschema));
+        //require_auth2(_self,N(modschema));
+        require_auth(_self);
 
         unifschemas u_schema(_self, _self);
 
@@ -214,7 +222,8 @@ namespace UnificationFoundation {
     }
 
     void unification_uapp::setschema(const uint64_t& pkey,const std::string& schema) {
-        require_auth2(_self,N(modschema));
+        //require_auth2(_self,N(modschema));
+        require_auth(_self);
 
         unifschemas u_schema(_self, _self);
 
@@ -233,7 +242,8 @@ namespace UnificationFoundation {
                                    const std::string& query,
                                    const uint8_t& price) {
 
-        require_auth2(_self,N(modreq));
+        //require_auth2(_self,N(modreq));
+        require_auth(_self);
 
         unifreqs data_requests(_self, _self);
 
@@ -255,7 +265,8 @@ namespace UnificationFoundation {
 
         //TODO - migrate to require_auth2 with custom permission level
         //require_auth(provider_name); //only provider can update this info
-        require_auth2(provider_name,N(modreq));
+        //require_auth2(provider_name,N(modreq));
+        require_auth(provider_name);
 
         unifreqs data_requests(_self, _self);
 
@@ -275,7 +286,8 @@ namespace UnificationFoundation {
     void unification_uapp::setrsakey(std::string rsa_key) {
 
         //Todo: need to verify this works - i.e. provider modifying consumer's contract using this permission
-        require_auth2(_self,N(modrsakey));
+        //require_auth2(_self,N(modrsakey));
+        require_auth(_self);
 
         unifrsakey _unifrsakey(_self, _self);
 
